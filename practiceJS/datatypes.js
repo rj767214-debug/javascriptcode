d3b897102ec2b9e3e60096af1cbd055ef1587d5d
rj767214-debug <rj767214@gmail.com>
2026-01-14 16:27:36 +0000

    datatype

---
+++ practiceJS/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newe version
+
+// alert (3+3) //we are using nodejs , not browser
+
+//console.log(5+6);// code readbility should be hogh
+
+
+console.log("rohit");
+
+let name = "rohit"
+let age = 23
+let isloggedin = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/ false
+//null => standalone value
+//undefined => value not assigned
+//symbol => unique
+
+
+//objects
+  
+console.log(typeof undefined); // undefined
+console.log(typeof null);      // object
